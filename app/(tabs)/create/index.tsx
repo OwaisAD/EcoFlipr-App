8dff043898625c7c33d49a50617d3672a44df45f
Owais Dashti <owaisdashti@outlook.com>
2024-04-16 19:54:08 +0200

    added tabs

--- app/(tabs)/create/index.tsx
+++ app/(tabs)/create/index.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
+import EditScreenInfo from "../../../components/EditScreenInfo";
+import { Text, View } from "../../../components/Themed";
 
-export default function HomeScreen() {
+export default function CreateScreen() {
   return (
-    <View className="bg-red-400 h-full">
-      <Text className="text-xl font-medium">Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Create</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
--- app/(tabs)/search/index.tsx
+++ app/(tabs)/search/index.tsx
@@ -1,12 +1,12 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from '../../../components/EditScreenInfo';
+import { Text, View } from '../../../components/Themed';
 
-export default function TabTwoScreen() {
+export default function SearchScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Search</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
@@ -18,6 +18,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#EEE",
+
   },
   title: {
     fontSize: 20,
